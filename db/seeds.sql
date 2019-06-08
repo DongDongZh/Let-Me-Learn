8d6a78b6555412cbd004899b51359fcd4f530526
Legionnaire <blake.marter@gmail.com>
2019-06-08 11:12:20 -0700

    adding schema and seeds for db testing

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+USE letMeLearn_db;
+
+CREATE TABLE student
+(
+  id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	type varchar(255) NOT null,
+  description varchar(8000) NOT null,
+  rating INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE teacher
+(
+  id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	type varchar(255) NOT null,
+  description varchar(8000) NOT null,
+  rating INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE task
+(
+  id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	category varchar(255) NOT null,
+  description varchar(8000) NOT null,
+  complete BOOLEAN default false,
+	PRIMARY KEY (id)
+);
