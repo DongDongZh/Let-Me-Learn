5ce56a5398d9c20958c35f0ec2cd7ee5dc8d6120
Legionnaire <blake.marter@gmail.com>
2019-06-10 20:19:47 -0700

    secrefyin db

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
-require(dotenv).config();
+require("dotenv").config();
 var db = {};
 
 if (config.use_env_variable) {
