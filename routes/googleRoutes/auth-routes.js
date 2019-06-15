2ce72c42845f118e72e0792beca33cfb1fccc158
kbub27 <kalebmerriman27@gmail.com>
2019-06-14 22:01:45 -0700

    added cookie session and logout route

--- routes/googleRoutes/auth-routes.js
+++ routes/googleRoutes/auth-routes.js
@@ -1,6 +1,13 @@
 var passport = require("passport");
+var cookieParser = require("cookie-parser");
+var cookieSession = require("cookie-session");
 
 module.exports = function (app) {
+  app.use(cookieSession({
+    name: "session",
+    keys: ["123"]
+  }));
+  app.use(cookieParser());
 
   app.get(
     "/auth/google",
@@ -11,18 +18,36 @@ module.exports = function (app) {
 
   app.get("/auth/logout", function (req, res) {
     //handle with passport
-    res.send("logging out");
+    req.logout();
+    req.session = null;
+    res.redirect("/");
   });
 
   app.get("/auth/google/callback", passport.authenticate("google"), function (
     req,
     res
   ) {
-    var loggedInUser = res.req.user.profile;
+    var loggedInUser = req.user.profile;
     console.log("Name: " + loggedInUser.displayName);
     console.log("Email: " + loggedInUser._json.email);
     console.log("Photo: " + loggedInUser._json.picture);
     console.log("GoogleId: " + loggedInUser.id);
+    req.session.token = req.user.token;
     res.redirect("/students");
+  });
+
+  app.get("/", function(req, res) {
+    console.log("why wont this log");
+    if (req.session.token) {
+      res.cookie("token", req.session.token);
+      res.json({
+        status: "session cookie set"
+      });
+    } else {
+      res.cookie("token", "");
+      res.json({
+        status: "session cookie not set"
+      });
+    }
   });
 };
