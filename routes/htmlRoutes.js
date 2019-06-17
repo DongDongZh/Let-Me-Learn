839233a4e46b8d48405e3a9a2b5e40f4c90941d8
Legionnaire <blake.marter@gmail.com>
2019-06-16 20:43:57 -0700

    testing git

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,40 +6,6 @@ module.exports = function (app) {
 
   // This request finds all Tasks with the Student associated and displays to the page
   app.get("/students", function (req, res) {
-<<<<<<< HEAD
-    res.sendFile(path.join(__dirname, "/../public/student_homepage.html"));
-    db.Task.findAll({}).then(function (dbTask) {
-      res.json(dbTask);
-      console.log("This is the front page: " + document.cookie);
-      
-    });
-  });
-
-  //******ROUTE HAS BEEN CHANGED*******
-  // This request finds a single Student with all their Tasks and displays to page (INACTIVE)
-  app.get("/students_browse/:id", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/student_browse.html"));
-    db.Student.findOne({ where: { id: req.params.id } })
-      .then(function (dbStudent) {
-        res.json(dbStudent);
-      });
-  });
-
-  // This request finds all Tasks with their associated Student and displays to the page
-  // app.get("/students_browse", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "/../public/student_homepage.html"));
-  //   db.Task.findAll({}).then(function (dbTask) {
-  //     res.json(dbTask);
-  //   });
-  // });
-
-  // This request gets the empty form to create a Task
-  app.get("/student_post", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/student_post.html"));
-    // db.Student.Task.findAll({}).then(function (dbTask) {
-    //   res.json(dbTask);
-    // });
-=======
     res.render("student_homepage");
   });
   // app.get("/students/:id", function (req, res) {
@@ -61,38 +27,10 @@ module.exports = function (app) {
 
   app.get("/student_post", function (req, res) {
     res.render("student_post");
->>>>>>> master
   });
 
   //  This request grabs all teachers and their 
   app.get("/teachers", function (req, res) {
-<<<<<<< HEAD
-    res.sendFile(path.join(__dirname, "../public/teacher_profiles.html"));
-    db.Teacher.findAll({}).then(function (dbTeacher) {
-      res.json(dbTeacher);
-    });
-  });
-
-  // This request finds one single teacher and displays their profile to the page.
-  app.get("/teachers/:id", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/teacher_profiles.html"));
-    db.Teacher.findOne({ where: { id: req.params.id } })
-      .then(function (dbTeacher) {
-        res.json(dbTeacher);
-      });
-  });
-  // This request should send an empty profile form to be filled out by the teacher
-  app.get("/teacher_post/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/teacher_post.html"));
-    // db.Example.findOne({ where: { id: req.params.id } }).then(function(
-    //   dbExample
-    // ) {
-    //   res.render("example", {
-    //     example: dbExample
-    //   });
-    // });
-  });
-=======
     res.render("teacher_browse");
   });
   // app.get("/teachers/:id", function (req, res) {
@@ -117,21 +55,13 @@ module.exports = function (app) {
   //   //   });
   //   // });
   // });
->>>>>>> master
 
   // Render 404 page for any unmatched routes
   // app.get("*", function(req, res) {
   //   res.sendFile("../public/404.html");
   // });
-<<<<<<< HEAD
-
-  app.get("/teacher_profile", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/teacher_profiles.html"));
-  }); 
-=======
   // app.get("/teacher_profile", function (req, res) {
   //   res.sendFile(path.join(__dirname, "/../public/teacher_profiles.html"));
   // }); 
->>>>>>> master
 };
 
